7c0c1a833dfbcc29d89b856b4171730daaba07e7
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-09-05 14:54:20 -0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Goodstores Base Customizations/Developments',
+    'category': 'Sale',
+    'summary': 'Custom',
+    'version': '1.0',
+    'description': """
+Core workflow customizations/developments.
+        """,
+    'depends': ['base','crm','sale'],
+    'data': [
+        'data/created_records.xml',
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_ui_views.xml',
+        'data/base_automation.xml',
+    ],
+    'installable': True,
+}
